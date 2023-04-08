86bf2c4c09d430fd382192275059f5cd6903a854
Karleen Richards <karleenmsrichards@gmail.com>
2023-04-08 19:43:34 +0100

    made minor changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 const moment = require("moment");
+const fs = require("fs");
 
 const port = process.env.PORT || 5050;
 
@@ -11,7 +12,11 @@ let date = new Date();
 let formattedDate = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
 app.get("/", function (request, response) {
-  response.send(`It's ${formattedDate}, thanks for visiting`);
+  response.send(
+    `It's ${formattedDate}, thanks for visiting and you are visitor number `
+  );
 });
+
+app.listen(port, () => console.log(`running port ${port}`));
 
 console.log(formattedDate);
