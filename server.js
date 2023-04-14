7b050473ca39771a1d812896aceb5f3cefaa2d93
Karleen Richards <karleenmsrichards@gmail.com>
2023-04-14 09:41:03 +0100

    made a few changes

--- server.js
+++ server.js
@@ -2,20 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-const moment = require("moment");
 
-const port = process.env.PORT || 5050;
+const port = 5050;
 
 let date = new Date();
 
-let formattedDate = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-
+let formattedDate = date.toLocaleTimeString();
 app.get("/", function (request, response) {
-  response.send(
-    `It's ${formattedDate}, thanks for visiting and you are visitor number `
-  );
+  response.json({ time: `${formattedDate}`, visitors: 0 });
 });
 
-app.listen(port, () => console.log(`running port ${port}`));
+app.listen(port, () => console.log(`running port ${port} 😎`));
 
 console.log(formattedDate);
